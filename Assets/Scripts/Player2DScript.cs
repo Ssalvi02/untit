f86b436934fd9d869ba5884f05635fb58093d4a6
sSalvi <jaosalvi@protonmail.com>
2023-05-09 14:35:45 -0300

    froggy spin
    frog flip
    spining frog
    sapu que jira
    helisaptero
    sapo gira foda

--- Assets/Scripts/Player2DScript.cs
+++ Assets/Scripts/Player2DScript.cs
@@ -9,8 +9,11 @@ public class Player2DScript : MonoBehaviour
     [SerializeField] private float move_speed = 10f;
     private Rigidbody2D rb;
     private Vector2 velref;
+    private Animator anim;
+
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -23,6 +26,8 @@ public class Player2DScript : MonoBehaviour
     {
         horiz_move = Input.GetAxisRaw("Horizontal");
         vert_move = Input.GetAxisRaw("Vertical");
+        anim.SetFloat("Horiz", horiz_move);
+        anim.SetFloat("Vert", vert_move);
         rb.velocity = Vector2.SmoothDamp(rb.velocity, new Vector2(horiz_move, vert_move).normalized * move_speed, ref velref, 0.05f);
     }
 
